ec5da872eb7f44229ec69bdccdbc3b49349bebc5
Alexander Eberts <alex.eberts@gmail.com>
2015-01-17 18:51:57 -0800

    Solved #58 Function Composition and #59 Juxtaposition

--- src/offline_4clojure/p58.clj
+++ src/offline_4clojure/p58.clj
@@ -3,11 +3,34 @@
 ;; tags - higher-order-functions:core-functions
 ;; restricted - comp
 (ns offline-4clojure.p58
-  (:use clojure.test))
+  (:use clojure.test)
+  (:use clojure.tools.trace))
+
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
+
+(defmacro dlet [bindings & body]
+  `(let [~@(mapcat (fn [[n v]]
+                     (if (or (vector? n) (map? n))
+                       [n v]
+                       [n v '_ `(println (name '~n) ":" ~v)]))
+                   (partition 2 bindings))]
+     ~@body))
 
 (def __
-;; your solution here
-)
+
+  (fn [& fargs]
+    (let [fns (vec (reverse fargs))]
+      (fn [& args]
+        (loop [fs (next fns) acc (apply (first fns) args)]
+          (if fs
+            (recur (next fs) ((first fs) acc))
+            acc)))))
+
+  )
+
 
 (deftest main-test []
   (are [soln] soln
@@ -16,3 +39,17 @@
 (= true ((__ zero? #(mod % 8) +) 3 5 7 9))
 (= "HELLO" ((__ #(.toUpperCase %) #(apply str %) take) 5 "hello world"))
 ))
+
+
+;(fn [& args]
+;  (let [fns (vec (reverse args))]
+;    (fn [arg]
+;      (loop [fs fns a arg]
+;        (if (empty? fs)
+;          a
+;          (recur (rest fs) ((first fn) a))
+;          )
+;        )
+;      )
+;    )
+;  )
--- src/offline_4clojure/p59.clj
+++ src/offline_4clojure/p59.clj
@@ -5,9 +5,49 @@
 (ns offline-4clojure.p59
   (:use clojure.test))
 
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
+
+(defmacro dlet [bindings & body]
+  `(let [~@(mapcat (fn [[n v]]
+                     (if (or (vector? n) (map? n))
+                       [n v]
+                       [n v '_ `(println (name '~n) ":" ~v)]))
+                   (partition 2 bindings))]
+     ~@body))
+
+(defn foo [& x]
+  {:x x :type (type (list x))}
+  )
+
+;amcnamara's solution:
+;
+;(fn [& f]
+;  (fn [& a]
+;    (map #(apply % a) f)))
+
+
+
+
 (def __
-;; your solution here
-)
+
+(fn [& fns]
+  (let [fs (list* fns)]
+    (fn [& args]
+      (reduce #(conj %1 (apply %2 args)) [] fs))))
+
+  )
+
+;(fn [& fargs]
+;  (let [fns fargs]
+;    (fn [& args]
+;      (loop [fs (rest fns) acc (dbg (into [] (apply (first fns) args)))]
+;        (if fs
+;          (recur (dbg (rest fs)) (dbg (conj acc (apply (first fs) args))))
+;          acc)))))
+
 
 (deftest main-test []
   (are [soln] soln
@@ -15,3 +55,8 @@
 (= ["HELLO" 5] ((__ #(.toUpperCase %) count) "hello"))
 (= [2 6 4] ((__ :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
 ))
+
+;dbg: ((__ + max min) 2 3 5 1 6 4 )
+;dbg: fns = (+ max min)
+;dbg: fs = '(+ max min)
+;dbg: args = 2 3 5 1 6 4
