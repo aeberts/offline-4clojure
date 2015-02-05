de858ed1245c89c7b3b4e1d72e033b6218f614d4
Alexander Eberts <alex.eberts@gmail.com>
2015-02-05 10:09:09 -0500

    Solved #67 - Primes

--- src/offline_4clojure/p67.clj
+++ src/offline_4clojure/p67.clj
@@ -6,9 +6,27 @@
 (ns offline-4clojure.p67
   (:use clojure.test))
 
+
 (def __
-;; your solution here
-)
+
+  (fn [x]
+    (let [prime? (fn [n]
+                   (let [div? (fn [div] (zero? (rem n div)))]
+                     (cond (<= n 3) (>= n 2) (or (div? 2) (div? 3)) false
+                           :else (loop [i 5]
+                                   (cond (> (* i i) n) true
+                                         (or (div? i) (div? (+ i 2))) false
+                                         :else (recur (+ i 6)))))))]
+      (loop [result [2 3] lst 4]
+        (if (= x (count result))
+          result
+          (if (prime? lst)
+            (recur (conj result lst) (inc lst))
+            (recur result (inc lst))))
+        )
+      )
+    )
+  )
 
 (deftest main-test []
   (are [soln] soln
@@ -16,3 +34,43 @@
 (= (__ 5) [2 3 5 7 11])
 (= (last (__ 100)) 541)
 ))
+
+
+(def __2
+
+  (fn [x]
+    (let [prime? (fn [n]
+                   (let [div? (fn [div] (zero? (rem n div)))]
+                     (cond (<= n 3) (>= n 2) (or (div? 2) (div? 3)) false
+                           :else (loop [i 5]
+                                   (cond (> (* i i) n) true
+                                         (or (div? i) (div? (+ i 2))) false
+                                         :else (recur (+ i 6)))))))
+          s (range 2 (+ 2 x))]
+      (filter #(prime? %) s )
+      )
+    )
+
+  )
+
+;; From Clojure-docs reduce
+;; Calculate primes until 1000
+
+;(reduce
+;  (fn [primes number]
+;    (if (some zero? (map (partial mod number) primes))
+;      primes
+;      (conj primes number)))
+;  [2]
+;  (take 1000 (iterate inc 3)))
+
+;jimm's solution:
+;
+;(fn [num]
+;  (letfn [(prime? [n]
+;                  (cond (< n 2) false
+;                        (== n 2) true
+;                        (even? n) false
+;                        :else (let [max-divisor-check (/ (- (int (Math/sqrt n)) 2) 2)] ; subtract 2 because we start iterating at 3
+;                                (not-any? #(zero? (unchecked-remainder n %)) (take max-divisor-check (iterate #(+ % 2) 3))))))]
+;    (take num (filter prime? (range)))))
