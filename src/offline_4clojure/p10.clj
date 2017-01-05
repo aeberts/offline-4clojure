2e673294861b01c3368ed30dececd0cad8241713
Alexander Eberts <zand@Alexanders-iMac.local>
2017-01-05 08:57:41 -0800

    Added solutions to p10, p11, p22 and p25. Updated to clojure 1.8

--- src/offline_4clojure/p10.clj
+++ src/offline_4clojure/p10.clj
@@ -1,17 +1,15 @@
 ;; Intro to Maps - Elementary
 ;; Maps store key-value pairs.  Both maps and keywords can be used as lookup functions. Commas can be used to make maps more readable, but they are not required.
-;; tags - 
-;; restricted - 
+;; tags -
+;; restricted -
 (ns offline-4clojure.p10
   (:use clojure.test))
 
 (def __
-  20
-)
+  20)
 
 (deftest main-test []
   (are [soln] soln
-(= __ ((hash-map :a 10, :b 20, :c 30) :b))
-(= __ (:b {:a 10, :b 20, :c 30}))
-))
+   (= __ ((hash-map :a 10, :b 20, :c 30) :b))
+   (= __ (:b {:a 10, :b 20, :c 30}))))
 
--- src/offline_4clojure/p22.clj
+++ src/offline_4clojure/p22.clj
@@ -7,13 +7,14 @@
 
 (def __
 ;; your solution here
+  (fn [coll]
+    (reduce (fn [c _] (inc c)) 0 coll))
 )
 
 (deftest main-test []
   (are [soln] soln
-(= (__ '(1 2 3 3 1)) 5)
-(= (__ "Hello World") 11)
-(= (__ [[1 2] [3 4] [5 6]]) 3)
-(= (__ '(13)) 1)
-(= (__ '(:a :b :c)) 3)
-))
+    (= (__ '(1 2 3 3 1)) 5)
+    (= (__ "Hello World") 11)
+    (= (__ [[1 2] [3 4] [5 6]]) 3)
+    (= (__ '(13)) 1)
+    (= (__ '(:a :b :c)) 3)))
