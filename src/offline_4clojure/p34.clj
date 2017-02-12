d414bba79271e2531f3462d8d9af4b864b9aa4f1
Alexander Eberts <alex.eberts@gmail.com>
2017-02-12 11:53:23 -0800

    Solution for p34 - Range

--- src/offline_4clojure/p34.clj
+++ src/offline_4clojure/p34.clj
@@ -6,12 +6,21 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+
+  ; Higher-order approach
+  (fn [start end]
+    (take (- end start) (iterate inc start)))
+
+  ; recursive approach:
+  ;(fn rng [start end]
+  ;  (if (>= start end)
+  ;    '()
+  ;    (cons start (rng (inc start) end))))
+  )
 
 (deftest main-test []
   (are [soln] soln
-(= (__ 1 4) '(1 2 3))
-(= (__ -2 2) '(-2 -1 0 1))
-(= (__ 5 8) '(5 6 7))
-))
+    (= (__ 1 4) '(1 2 3))
+    (= (__ -2 2) '(-2 -1 0 1))
+    (= (__ 5 8) '(5 6 7))
+    ))
