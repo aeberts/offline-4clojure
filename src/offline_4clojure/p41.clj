63f960070f21c7c8c8e733255cab803e77492c86
Alexander Eberts <alex.eberts@gmail.com>
2017-02-21 23:50:43 -0800

    Solutions for p41 - Drop every Nth

--- src/offline_4clojure/p41.clj
+++ src/offline_4clojure/p41.clj
@@ -5,13 +5,25 @@
 (ns offline-4clojure.p41
   (:use clojure.test))
 
+; Solution using "traditional" keep-indexed (could also use map-indexed)
+
+;(def __
+;  (fn [coll n]
+;    (keep-indexed
+;      (fn [idx itm]
+;        (if (not (= 0 (mod (inc idx) n)))
+;          itm))
+;      coll)))
+
+; Sexier solution using partition-all:
+
 (def __
-;; your solution here
-)
+  (fn [coll n]
+    (apply concat (partition-all (dec n) n coll))))
 
 (deftest main-test []
   (are [soln] soln
-(= (__ [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
-(= (__ [:a :b :c :d :e :f] 2) [:a :c :e])
-(= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6])
-))
+    (= (__ [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
+    (= (__ [:a :b :c :d :e :f] 2) [:a :c :e])
+    (= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6])
+    ))
