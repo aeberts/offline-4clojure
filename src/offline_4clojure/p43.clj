19898f743cb738b3af911feaf897613877134443
Alex Eberts <alex.eberts@gmail.com>
2018-12-23 20:09:48 -0700

    Solved p43 Reverse Interleave

--- src/offline_4clojure/p43.clj
+++ src/offline_4clojure/p43.clj
@@ -6,12 +6,24 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+
+  (fn [coll n]
+    (loop [it n
+           result '()
+           c coll]
+      (if (= it 0)
+        (reverse result)
+        (recur (- it 1)
+               (conj result (take-nth n c))
+               (rest c)))
+      )
+    )
+  )
 
 (deftest main-test []
   (are [soln] soln
-(= (__ [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
-(= (__ (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
-(= (__ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
-))
+    (= (__ [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
+    (= (__ (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
+    (= (__ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+    ))
+
